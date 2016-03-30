66733b7552eae93e48e2e9a0f2d8a085de7e8109
Olli Jarva <olli.jarva@futurice.com>
2016-03-30 20:16:45 +0300

    Refactor. Improve logging.

--- server.py
+++ server.py
@@ -6,6 +6,7 @@ import redis
 import socket
 import threading
 import time
+import logging
 
 UDP_IP = "0.0.0.0"
 UDP_PORT = 8225
@@ -13,6 +14,7 @@ UDP_PORT = 8225
 
 class StatsReceiver(object):
     TEMPERATURE_RE = re.compile(r"^.*-t\d$")
+    POWER_READING_RE = re.compile(r"^E\d+")
     PIR_RE = re.compile("^.*pir$")
 
     def __init__(self):
@@ -25,6 +27,13 @@ class StatsReceiver(object):
         self.redis = redis.StrictRedis()
         self.running = False
         self.node_value_sets = {}
+        self.logger = logging.getLogger("udp-node-receiver")
+        self.logger.setLevel(logging.INFO)
+        format_string = "%(asctime)s - {ip}:{port} - %(levelname)s - %(message)s".format(ip=UDP_IP, port=UDP_PORT)
+        formatter = logging.Formatter(format_string)
+        ch = logging.StreamHandler()
+        ch.setFormatter(formatter)
+        self.logger.addHandler(ch)
 
     def send_timer(self):
         last_sent_at = time.time()
@@ -39,7 +48,58 @@ class StatsReceiver(object):
                 except Queue.Empty:
                     break
             if len(items) > 0:
+                self.logger.debug("Sending", items)
                 self.redis.publish("influx-update-pubsub", json.dumps(items))
+
+    def process(self, node_name, key, value):
+        item_type = "generic"
+        fields = {}
+        if self.TEMPERATURE_RE.match(key):
+            value = float(value) / 100
+            item_type = "temperature"
+            self.logger.debug("Found temperature %s, /100", value)
+        elif self.PIR_RE.match(key):
+            value = value == "1"
+            item_type = "pir"
+            self.logger.debug("Found PIR: %s", value)
+        elif self.POWER_READING_RE.match(key):
+            value = float(value) * 230
+            item_type = "watts"
+            self.logger.debug("Found amps, converted to watts: %s", value)
+        elif node_name == "tea" and key == "nfc-id":
+            item_type = "tea-reader"
+            self.logger.debug("Found NFC tag")
+        fields[item_type] = value
+
+        output = {
+            "time": datetime.datetime.utcnow().isoformat() + "Z",
+            "measurement": "node-" + node_name,
+            "tags": {
+                "key": key,
+            },
+            "fields": fields,
+        }
+        if node_name not in self.node_value_sets:
+            self.node_value_sets[node_name] = {}
+        self.node_value_sets[node_name][key] = value
+        self.redis.publish("watchdog-input", json.dumps({"name": "node-%s" % node_name, "values": self.node_value_sets[node_name]}))
+        if item_type == "pir":
+            if value:
+                self.redis.publish("lightcontrol-triggers-pubsub", json.dumps({"key": node_name, "trigger": "pir"}))
+            else:
+                self.logger.info("Didn't trigger lightcontrol - PIR reading is False")
+            if key not in self.last_values:
+                self.last_values[key] = {"value": None, "seen": datetime.datetime.now() - datatime.timedelta(seconds=31)}
+            time_diff = datetime.datetime.now() - self.last_values[key]["seen"]
+            if self.last_values[key]["value"] == value and time_diff < datetime.timedelta(seconds=30):
+                self.logger.debug("Skip sending %s - value didn't change and last seen %ss ago", output, time_diff.total_seconds())
+                return
+            self.last_values[key]["value"] = value
+            self.last_values[key]["seen"] = datetime.datetime.now()
+        elif item_type == "tea-reader":
+            self.redis.publish("tea-reader-pubsub", json.dumps({"id": value}))
+
+        self.send_queue.put(output)
 
     def run(self):
         try:
@@ -50,49 +110,23 @@ class StatsReceiver(object):
                 data, addr = self.sock.recvfrom(1024)  # buffer size is 1024 bytes
                 splitted_data = data.split(":")
                 if len(splitted_data) != 2:
-                    print "Malformed data from {sender}: {data}".format(sender=addr, data=data)
+                    self.logger.warning("Malformed data from %s: %s", addr, data)
                     continue
                 key = splitted_data[0]
                 splitted_key = key.split(".")
                 node_name = splitted_key[0]
                 if len(splitted_key) < 2:
-                    print "Malformed key from {sender}: {data}".format(sender=addr, data=data)
+                    self.logger.warning("Malformed key from %s: %s", addr, data)
                     continue
                 value = splitted_data[1].strip()
-                key = ".".join(splitted_key[1:])
-                item_type = "generic"
-                fields = {}
-                if self.TEMPERATURE_RE.match(key):
-                    value = float(value) / 100
-                    item_type = "temperature"
-                elif self.PIR_RE.match(key):
-                    value = value == "1"
-                    item_type = "pir"
-                fields[item_type] = value
-
-                output = {
-                    "time": datetime.datetime.utcnow().isoformat() + "Z",
-                    "measurement": "node-" + node_name,
-                    "tags": {
-                        "key": key,
-                    },
-                    "fields": fields,
-                }
-                if node_name not in self.node_value_sets:
-                    self.node_value_sets[node_name] = {}
-                self.node_value_sets[node_name][key] = value
-                self.redis.publish("watchdog-input", json.dumps({"name": "node-%s" % node_name, "values": self.node_value_sets[node_name]}))
-                if item_type == "pir":
-                    if value:
-                        self.redis.publish("lightcontrol-triggers-pubsub", json.dumps({"key": node_name, "trigger": "pir"}))
-                    if key not in self.last_values:
-                        self.last_values[key] = {"value": None, "seen": datetime.datetime.now()}
-                    if self.last_values[key]["value"] == value and datetime.datetime.now() - self.last_values[key]["seen"] < datetime.timedelta(seconds=120):
-                        continue
-                    self.last_values[key]["value"] = value
-                    self.last_values[key]["seen"] = datetime.datetime.now()
-
-                self.send_queue.put(output)
+                if "|" in value:
+                    values = value.split("|")
+                    for value in values:
+                        item_key, item_value = value.split("=")
+                        self.process(node_name, item_key, item_value)
+                else:
+                    key = ".".join(splitted_key[1:])
+                    self.process(node_name, key, value)
         except KeyboardInterrupt as err:
             self.running = False
             raise err
